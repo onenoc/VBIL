65d0e4c06736bd256a05dae462c5c64eb6e215ef
Alexander Moreno <onenoc@lawn-128-61-67-90.lawn.gatech.edu>
2015-12-15 23:45:27 -0500

    the beta posterior bernoulli likelihood sort of works

--- paper_example.py
+++ paper_example.py
@@ -12,14 +12,22 @@ def fisher_info(params):
     return I
 
 def inv_fisher(params):
+    #print "params"
+    #print params
     alpha = params[0]
     beta = params[1]
     I = np.zeros((2,2))
     a=special.polygamma(1,alpha)-special.polygamma(1,alpha+beta)
+    #print "inv"
+    #print a
     b=special.polygamma(1,alpha+beta)
+    #print b
     c=special.polygamma(1,alpha+beta)
+    #print c
     d=special.polygamma(1,beta)-special.polygamma(1,alpha+beta)
+    #print d
     const = 1./(a*d-b*c)
+    #print const
     I[0][0]=d
     I[0][1]=-b
     I[1][0]=-c
@@ -35,27 +43,36 @@ def H(params,n,k):
     H[1]=(n-k)*special.polygamma(1,beta)-n*special.polygamma(1,alpha+beta)
     return H
 
-def iterate(params,i):
-    a = 1./(5+i)
+def iterate(logparams,i):
+    params = np.exp(logparams)
+    a = 1./(50000+i)
     H_val = H(params,200,57)
     fisher = fisher_info(params)
-    params = params-a*(np.dot(fisher,params)-H_val)
-    return params
+    logparams = logparams-a*(np.dot(fisher,params)-H_val)*params
+    return logparams
 
-def iterate_nat_grad(params,i):
-    a = 1./(5+i)
+def iterate_nat_grad(logparams,i):
+    params = np.exp(logparams)
+    a = 1./(3000000000+3000000000*i)
     H_val = H(params,200,57)
-    params = (1-a)*params+a*np.dot(inv_fisher(params),H_val)
-    return params
+    logparams = logparams+a*(params-np.dot(inv_fisher(params),H_val))*params
+    return logparams
     
 
 if __name__=='__main__':
-    params = np.array([100.,30.])
-    for i in range(3000000):
-       params = iterate(params,i)
+    logparams = np.array([np.random.randint(0,10),np.random.randint(0,10)])
+    for i in range(1,500000):
+       logparams = iterate(logparams,i)
        if i%1000==0:
+           params = np.exp(logparams)
            print params
            alpha = params[0]
            beta = params[1]
            print "mean is %f" % (alpha/(alpha+beta))
-    
+    '''
+    print "params"
+    print params 
+    alpha = params[0]
+    beta = params[1]
+    print "mean is %f" % (alpha/(alpha+beta))
+    '''
